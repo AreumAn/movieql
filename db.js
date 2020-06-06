a4e41a20af27ff6d40d50f39f2e8b09761da31e1
AREUM AN <areum.an.0928@gmail.com>
2020-06-06 09:11:14 -0400

    Update getMovies using params

--- db.js
+++ db.js
@@ -4,18 +4,16 @@ const MOVIE_LIST = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAIL = `${BASE_URL}movie_details.json`;
 
 export const getMovies = async (limit, rating) => {
-  let REQUEST_URL = MOVIE_LIST;
-  if (limit > 0) {
-    REQUEST_URL += `?limit=${limit}`;
-  }
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
-  }
   const {
     data: {
       data: { movies },
     },
-  } = await axios(REQUEST_URL);
+  } = await axios(MOVIE_LIST, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
   return movies;
 };
 
